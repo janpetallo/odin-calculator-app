9b0ae4a14de148405e8744004640f2ee8ae195f5
janpetallo <101074771+janpetallo@users.noreply.github.com>
2024-04-23 17:41:09 -0600

    starter files with operations and functions

---
+++ script.js
@@ -0,0 +1,37 @@
+
+
+function add(firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
+}
+
+function subtract(firstNumber, secondNumber) {
+  return firstNumber - secondNumber;
+}
+
+function multiply(firstNumber, secondNumber) {
+  return firstNumber * secondNumber;
+}
+
+function divide(firstNumber, secondNumber) {
+  return firstNumber / secondNumber;
+}
+
+function operate(operator, firstNumber, secondNumber){
+    if(operator === '+'){
+        return add(firstNumber, secondNumber);
+    } else if (operator === '-') {
+        return subtract(firstNumber, secondNumber);
+    } else if(operator === '*') {
+        return multiply(firstNumber, secondNumber)
+    } else if (operator === '/') {
+        return divide(firstNumber, secondNumber)
+    }
+}
+
+// variables for the calculator
+let firstNumber = '';
+let secondNumber = '';
+let operator = '';
+
+
+
