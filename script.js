6af3d295b56554f01eb4a32946ef3f6ded50ca43
janpetallo <101074771+janpetallo@users.noreply.github.com>
2024-04-23 19:27:36 -0600

    implemented operation and display of the result

--- script.js
+++ script.js
@@ -22,9 +22,9 @@ function operate(operator, firstNumber, secondNumber){
     } else if (operator === '-') {
         return subtract(firstNumber, secondNumber);
     } else if(operator === '*') {
-        return multiply(firstNumber, secondNumber)
+        return multiply(firstNumber, secondNumber);
     } else if (operator === '/') {
-        return divide(firstNumber, secondNumber)
+        return divide(firstNumber, secondNumber);
     }
 }
 
@@ -38,11 +38,33 @@ let displayValue = '0'; // initialize display value
 // function to update display
 function updateDisplay(e) {
     const btnValue = e.target.innerText;
-    if (e.target.id === 'digit') {
-        if (displayValue === '0') {
+    if (e.target.classList.contains('digit')) { 
+        if (operator !== '' && secondNumber === '') { // if operator is selected and second number is empty
             displayValue = btnValue;
-        } else {
+            secondNumber = Number(displayValue);
+        } else if (operator !== '' && secondNumber !== '') { // if operator is selected and second number is not empty
             displayValue += btnValue;
+            secondNumber = Number(displayValue);
+        } else if (displayValue === '0') { // if display value is 0
+            displayValue = btnValue;
+        } else { // if display value is not 0
+            displayValue += btnValue;
+        }
+    } else if (e.target.classList.contains('operation')) {
+        if (firstNumber === '' || secondNumber === '') {  
+            firstNumber = Number(displayValue);
+            if (btnValue === 'x') {
+                operator = '*';
+            } else if (btnValue === '÷') {
+                operator = '/';
+            } else {
+                operator = btnValue; 
+            }
+        } else if (secondNumber !== '') { 
+            firstNumber = operate(operator, firstNumber, secondNumber);
+            operator = btnValue;
+            displayValue = String(firstNumber);
+            secondNumber = ''; 
         }
     }
     document.querySelector('.display-screen').value = displayValue;
